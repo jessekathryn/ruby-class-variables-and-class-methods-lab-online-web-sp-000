2810a5deb65901455f1ba4319ff5b592804f48b5
Jesse Barnes <jessekathrynbarnes@gmail.com>
2019-05-02 02:05:12 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,32 @@
+class Song 
+
+attr_accessor :name, :artist, :genre 
+
+@@count = 0 
+ @@artists = []
+  @@genres = []
+
+def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count += 1
+    @@artists << artist
+    @@genres << genre
+  end
+
+def self.count
+  @@count
+ end 
+
+def self.artists
+   @@artists << artists
+ end
+ end
+
+def genres(genre)
+if !@@genres.include?(genre) 
+   @@genres << genre
+  end
+end
+end
