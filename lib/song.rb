2a6a507758c4145f0c0cf04da050b88506b93d1b
Jesse Barnes <jessekathrynbarnes@gmail.com>
2019-05-02 03:32:29 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -40,15 +40,21 @@ def self.genre_count
  end
 
 def self.artist_count
- final_genre_count = {}
-  @@genres.collect do |genre|
-    if !final_genre_count[genre] 
-      final_genre_count[genre] = 1
+  #you have to create a new hash to store the array
+ final_artist_count = {}
+  #iterate through the class variable artists holding the array
+  @@artists.collect do |artist|
+    #use artist as the key 
+    if !final_artist_count[artist] 
+      #this is where you want to ask the HASH if the key artist is there or not
+      final_artist_count[artist] = 1
+       #if it is there, we return a key and value as 1
     else
-      final_genre_count[genre] += 1
+      final_artist_count[artist] += 1
+       #if it isn't there then we store the value and add 1
    end
   end
- final_genre_count
+ final_artist_count #return the final hash after it has iterated through artists class variable
  end
 end 
  
